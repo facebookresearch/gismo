e6d6aba1ba40b285fa8bd9d03ce927d17a7b5a8f
QuentinDuval <QuentinDuval@users.noreply.github.com>
2020-12-07 16:47:59 -0500

    [refactor] separate the raw configuration read from the config directory and the configuration used for the experiment

--- inv_cooking/config/config.py
+++ inv_cooking/config/config.py
@@ -1,22 +1,14 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from enum import Enum
-from typing import Dict, Tuple
 
-from hydra.core.config_store import ConfigStore
-from omegaconf import MISSING, OmegaConf, DictConfig
+from omegaconf import MISSING
 
 from .dataset import DatasetConfig
 from .image_encoder import ImageEncoderConfig
-from .ingredient_predictor import (
-    IngredientPredictorConfig,
-    IngredientPredictorFFConfig,
-    IngredientPredictorLSTMConfig,
-    IngredientPredictorTransformerConfig
-)
+from .ingredient_predictor import IngredientPredictorConfig
 from .optimization import OptimizationConfig
 from .recipe_generator import RecipeGeneratorConfig
 from .slurm import SlurmConfig
-from .utils import untyped_config
 
 
 class TaskType(Enum):
@@ -28,19 +20,6 @@ class TaskType(Enum):
 @dataclass
 class CheckpointConfig:
     dir: str = MISSING
-
-
-@dataclass
-class Experiment:
-    comment: str = ""
-    optimization: OptimizationConfig = OptimizationConfig()
-
-
-@dataclass
-class Experiments:
-    im2ingr: Dict[str, Experiment] = field(default_factory=dict)
-    im2recipe: Dict[str, Experiment] = field(default_factory=dict)
-    ingr2recipe: Dict[str, Experiment] = field(default_factory=dict)
 
 
 @dataclass
@@ -60,62 +39,3 @@ class Config:
     ingr_predictor: IngredientPredictorConfig = MISSING
     slurm: SlurmConfig = SlurmConfig()
 
-
-@dataclass
-class RawConfig:
-    """
-    The schema of the raw configuration as read by hydra:
-    * it contains the full DB of experiment
-    * it contains the information needed to select the experiment
-    This configuration is then transformed to be the configuration of one experiment.
-    """
-    task: str = MISSING
-    name: str = MISSING
-    recipe_gen: RecipeGeneratorConfig = RecipeGeneratorConfig()
-    image_encoder: ImageEncoderConfig = ImageEncoderConfig()
-    ingr_predictor: DictConfig = untyped_config()
-    checkpoint: CheckpointConfig = CheckpointConfig()
-    dataset: DatasetConfig = DatasetConfig()
-    slurm: SlurmConfig = SlurmConfig()
-    experiments: Experiments = Experiments()
-
-    @classmethod
-    def to_config(cls, raw_config: 'RawConfig') -> Config:
-        experiment_name, experiment = cls._get_experiment(raw_config)
-        config = OmegaConf.structured(Config)
-        config.task = raw_config.task
-        config.name = experiment_name
-        config.comment = experiment.comment
-        config.slurm = raw_config.slurm
-        config.dataset = raw_config.dataset  # TODO - search the right dataset
-        config.recipe_gen = raw_config.recipe_gen
-        config.image_encoder = raw_config.image_encoder
-        config.ingr_predictor = cls._get_ingr_predictor(raw_config.ingr_predictor)
-        config.checkpoint = raw_config.checkpoint
-        config.optimization = experiment.optimization
-        return config
-
-    @staticmethod
-    def _get_experiment(raw_config: 'RawConfig') -> Tuple[str, Experiment]:
-        experiment_name = raw_config.name or raw_config.task
-        experiment = raw_config.experiments[raw_config.task][experiment_name]
-        return experiment_name, experiment
-
-    @staticmethod
-    def _get_ingr_predictor(ingr_predictor: DictConfig) -> IngredientPredictorConfig:
-        model = ingr_predictor.model
-        if model == "ff_bce":
-            schema = OmegaConf.structured(IngredientPredictorFFConfig)
-            return OmegaConf.merge(schema, ingr_predictor)
-        elif model == "lstmset":
-            schema = OmegaConf.structured(IngredientPredictorLSTMConfig)
-            return OmegaConf.merge(schema, ingr_predictor)
-        elif model == "tf":
-            schema = OmegaConf.structured(IngredientPredictorTransformerConfig)
-            return OmegaConf.merge(schema, ingr_predictor)
-        else:
-            raise ValueError(f"Invalid ingredient predictor model {model}")
-
-
-cs = ConfigStore.instance()
-cs.store(name="config", node=RawConfig)
