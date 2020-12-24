9ced525c787e421e8f1ae11305bdaf040166324e
QuentinDuval <QuentinDuval@users.noreply.github.com>
2020-12-24 09:16:06 -0500

    [feat] switch from DP mode to DDP mode

--- inv_cooking/scheduler/scheduler.py
+++ inv_cooking/scheduler/scheduler.py
@@ -4,6 +4,7 @@ from typing import List
 
 import hydra
 import submitit
+import torch
 
 from inv_cooking.config import Config
 from inv_cooking.trainer import run_training
@@ -13,9 +14,9 @@ def schedule_jobs(configurations: List[Config]) -> None:
     _copy_source_code_to_cwd()  # Because Hydra create a new running folder
     for config in configurations:
         if config.slurm.partition == "local":
-            run_training(config, gpus=2, nodes=1, distributed_mode="dp")
+            _schedule_job_locally(config)
         else:
-            _schedule_job_on_slurm_using_dp(config)
+            _schedule_job_on_slurm(config)
 
 
 def _copy_source_code_to_cwd():
@@ -34,10 +35,12 @@ def _copy_source_code_to_cwd():
     print(f"Running in folder {target_path}")
 
 
-def _schedule_job_on_slurm_using_dp(cfg: Config):
-    """
-    Run the job whose configuration is given as parameter on SLURM
-    """
+def _schedule_job_locally(cfg: Config):
+    nb_gpu = torch.cuda.device_count()
+    run_training(cfg, gpus=nb_gpu, nodes=1, distributed_mode="ddp")
+
+
+def _schedule_job_on_slurm(cfg: Config):
     nb_gpus = cfg.slurm.gpus_per_node
     executor = submitit.AutoExecutor(folder=cfg.checkpoint.log_folder)
     executor.update_parameters(
@@ -48,9 +51,9 @@ def _schedule_job_on_slurm_using_dp(cfg: Config):
         timeout_min=cfg.slurm.timeout_min,
         nodes=cfg.slurm.nodes,
         cpus_per_task=cfg.slurm.cpus_per_task,
-        tasks_per_node=1,
+        tasks_per_node=nb_gpus,
         gpus_per_node=nb_gpus,
         mem_gb=cfg.slurm.mem_by_gpu * nb_gpus,
     )
-    job = executor.submit(run_training, cfg, nb_gpus, cfg.slurm.nodes, "dp")
+    job = executor.submit(run_training, cfg, nb_gpus, cfg.slurm.nodes, "ddp")
     print(f"Submitted {job.job_id}")
