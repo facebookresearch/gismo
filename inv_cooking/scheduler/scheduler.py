1638dd53a0df08700057b67997315bfa08bfec11
QuentinDuval <QuentinDuval@users.noreply.github.com>
2021-01-08 11:11:52 -0500

    [refactor] (hydra) extract hydra utils in a different file to make them reusable for some tests

--- inv_cooking/scheduler/scheduler.py
+++ inv_cooking/scheduler/scheduler.py
@@ -1,38 +1,20 @@
-import os
-import shutil
 from typing import List
 
-import hydra
 import submitit
 import torch
 
 from inv_cooking.config import Config
 from inv_cooking.training import run_training
+from inv_cooking.utils.hydra import copy_source_code_to_cwd
 
 
 def schedule_jobs(configurations: List[Config]) -> None:
-    _copy_source_code_to_cwd()  # Because Hydra create a new running folder
+    copy_source_code_to_cwd()  # Because Hydra create a new running folder
     for config in configurations:
         if config.slurm.partition == "local":
             _schedule_job_locally(config)
         else:
             _schedule_job_on_slurm(config)
-
-
-def _copy_source_code_to_cwd():
-    """
-    Copy the important folder to the path in which hydra will be running the code
-    * this allows relative path to continue to work
-    * this allows submitit to find the main module
-    """
-    original_path = hydra.utils.get_original_cwd()
-    target_path = os.getcwd()
-    folders_to_copy = ["inv_cooking", "data"]
-    for folder in folders_to_copy:
-        src_folder = os.path.join(original_path, folder)
-        dst_folder = os.path.join(target_path, folder)
-        shutil.copytree(src_folder, dst_folder)
-    print(f"Running in folder {target_path}")
 
 
 def _schedule_job_locally(cfg: Config):
---
+++ inv_cooking/utils/hydra.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+import hydra
+
+
+def copy_source_code_to_cwd():
+    """
+    Copy the important folder to the path in which hydra will be running the code
+    * this allows relative path to continue to work
+    * this allows submitit to find the main module
+    """
+    original_path = hydra.utils.get_original_cwd()
+    target_path = os.getcwd()
+    folders_to_copy = ["inv_cooking", "data"]
+    for folder in folders_to_copy:
+        src_folder = os.path.join(original_path, folder)
+        dst_folder = os.path.join(target_path, folder)
+        shutil.copytree(src_folder, dst_folder)
+    print(f"Running in folder {target_path}")
