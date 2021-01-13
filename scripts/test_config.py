1546785086494aef0818e82ccb43d5804622ec72
QuentinDuval <QuentinDuval@users.noreply.github.com>
2021-01-13 10:16:48 -0500

    [feat] (script) to visualize the configuration instead of running the full job

--- scripts/test_config.py
+++ scripts/test_config.py
@@ -1,19 +1,18 @@
-import os
-
 import hydra
 from omegaconf import OmegaConf
 
 from inv_cooking.scheduler import RawConfig
 
 
-@hydra.main(config_path="conf", config_name="config")
+@hydra.main(config_path="../conf", config_name="config")
 def main(cfg: RawConfig) -> None:
-    print(os.getcwd())
-    print(OmegaConf.to_yaml(cfg))
+    """
+    Print all job configurations that are generated from a given task and experiment name
+    """
     configs = RawConfig.to_config(cfg)
     for i, config in enumerate(configs):
         print("-" * 50)
-        print(f"RUN {i}")
+        print(f"EXPERIMENT {i+1}")
         print(OmegaConf.to_yaml(config))
 
 
