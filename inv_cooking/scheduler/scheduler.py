9caf02c759ded55853e310b4140d3ef9264cce0b
Adriana Romero Soriano <adrianars@fb.com>
2021-01-22 07:21:51 -0800

    [fix] post PR review changes to evaluation

--- inv_cooking/scheduler/scheduler.py
+++ inv_cooking/scheduler/scheduler.py
@@ -8,18 +8,18 @@ from inv_cooking.training import run_training, run_eval
 from inv_cooking.utils.hydra import copy_source_code_to_cwd
 
 
-def schedule_jobs(configurations: List[Config], mode: str) -> None:
+def schedule_jobs(configurations: List[Config], training_mode: bool) -> None:
     copy_source_code_to_cwd()  # Because Hydra create a new running folder
     for config in configurations:
         if config.slurm.partition == "local":
-            _schedule_job_locally(config, mode)
+            _schedule_job_locally(config, training_mode)
         else:
-            _schedule_job_on_slurm(config, mode)
+            _schedule_job_on_slurm(config, training_mode)
 
 
-def _schedule_job_locally(cfg: Config, mode: str):
+def _schedule_job_locally(cfg: Config, training_mode: bool):
     nb_gpu = torch.cuda.device_count()
-    if mode == "train":
+    if training_mode:
         run_training(
             cfg, gpus=nb_gpu, nodes=1, distributed_mode="ddp", load_checkpoint=False
         )
@@ -27,7 +27,7 @@ def _schedule_job_locally(cfg: Config, mode: str):
         run_eval(cfg, gpus=nb_gpu, nodes=1, distributed_mode="ddp")
 
 
-def _schedule_job_on_slurm(cfg: Config, mode: str):
+def _schedule_job_on_slurm(cfg: Config, training_mode: bool):
     nb_gpus = cfg.slurm.gpus_per_node
     executor = submitit.AutoExecutor(folder=cfg.checkpoint.log_folder)
     executor.update_parameters(
@@ -43,7 +43,7 @@ def _schedule_job_on_slurm(cfg: Config, mode: str):
         mem_gb=cfg.slurm.mem_by_gpu * nb_gpus,
     )
 
-    if mode == "train":
+    if training_mode:
         trainer = ResumableTrainer(config=cfg, nb_gpu=nb_gpus, nb_node=cfg.slurm.nodes)
         job = executor.submit(
             trainer,
--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from inv_cooking.scheduler import schedule_jobs, RawConfig
 
 @hydra.main(config_path="conf", config_name="config")
 def main(cfg: RawConfig) -> None:
-    schedule_jobs(RawConfig.to_config(cfg), mode="train")
+    schedule_jobs(RawConfig.to_config(cfg), training_mode=True)
 
 
 if __name__ == "__main__":
