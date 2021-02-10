5bdc5142132f4410e624b9c1004621d0a40b1a8c
Adriana Romero Soriano <adrianars@fb.com>
2021-02-10 14:22:11 -0800

    [fix] call schedule_jobs without to_config function

--- eval.py
+++ eval.py
@@ -5,7 +5,7 @@ from inv_cooking.scheduler import schedule_jobs, RawConfig
 
 @hydra.main(config_path="conf", config_name="config")
 def main(cfg: RawConfig) -> None:
-    schedule_jobs(RawConfig.to_config(cfg), training_mode=False)
+    schedule_jobs(cfg, training_mode=False)
 
 
 if __name__ == "__main__":
