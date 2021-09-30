53daa82d451ef2421d3c86646674cfe6ac9280ac
QuentinDuval <QuentinDuval@users.noreply.github.com>
2021-09-30 16:37:12 -0400

    [log] a few additional logs for the pre-processing

--- preprocess.py
+++ preprocess.py
@@ -15,9 +15,27 @@ def main(cfg: RawConfig) -> None:
     Example usage:
     `python preprocess.py dataset=recipe1m`
     """
-    vocab_ingrs, dataset = run_dataset_pre_processing(cfg.dataset.path, cfg.dataset.pre_processing)
-    train_subs, val_subs, test_subs = run_comment_pre_processing(cfg.dataset.path, cfg.dataset.pre_processing.save_path, vocab_ingrs, dataset)
+
+    print("Start running: run_dataset_pre_processing...")
+    vocab_ingrs, dataset = run_dataset_pre_processing(
+        cfg.dataset.path,
+        cfg.dataset.pre_processing
+    )
+    print("Done running: run_dataset_pre_processing!")
+
+    print("Start running: run_comment_pre_processing...")
+    train_subs, val_subs, test_subs = run_comment_pre_processing(
+        cfg.dataset.path,
+        cfg.dataset.pre_processing.save_path,
+        vocab_ingrs,
+        dataset
+    )
+    print("Done running: run_comment_pre_processing!")
+
+    print("Start running: create_pre_processed_recipesubs_data...")
     create_pre_processed_recipesubs_data(cfg.dataset.pre_processing.save_path, dataset, {"train": train_subs, "val": val_subs, "test": test_subs})
+    print("Done running: create_pre_processed_recipesubs_data!")
+
 
 if __name__ == "__main__":
     main()
