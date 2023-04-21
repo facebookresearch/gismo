970b13c04da971614f03041a9b6f66d0b3dc4d18
Adriana Romero Soriano <adrianars@fb.com>
2023-04-21 07:54:27 -0700

    header cpw

--- inv_cooking/config/ingredient_predictor.py
+++ inv_cooking/config/ingredient_predictor.py
@@ -1,4 +1,5 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates 
+# Copyright (c) 
+# Meta Platforms, Inc. and affiliates 
 # All rights reserved.
 #
 # This source code is licensed under the license found in the
